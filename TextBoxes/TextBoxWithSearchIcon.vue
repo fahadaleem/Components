28c588cb4a8410f7cfa6a38cafa5ab67d9318917
fahadaleem <faleem396@gmail.com>
2021-11-02 00:08:23 +0500

    Textbox with search icon created and added.

---
+++ TextBoxes/TextBoxWithSearchIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="pl display-flex align-items-center" id="search-container">
+    <i class="bx bx-search" id="search-icon"></i>
+    <input
+      type="text"
+      class="form-field"
+      id="search-field"
+      placeholder="Search"
+    />
+  </div>
+</template>
+<style scoped>
+#search-container {
+  position: relative;
+}
+#search-field {
+  padding-left: 2rem !important;
+}
+
+#search-icon {
+  position: absolute;
+  left: 2rem;
+}
+</style>
